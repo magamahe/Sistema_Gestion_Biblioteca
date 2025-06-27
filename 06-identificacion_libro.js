5c25a90e219646d731e40bb342e7316f90ef9c7f
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-06-27 20:05:33 -0300

    eliminación de acento en titulo

--- 06-identificacion_libro.js
+++ 06-identificacion_libro.js
@@ -4,9 +4,9 @@ const libros = require('./01-lista_libros.js');
 
 function librosConPalabrasEnTitulo(){
     const resultado = libros
-        .filter(libro => /^[a-zA-Z\s]+$/.test(libro.título) //filtra los libros cuyo título contiene solo letras y espacios 
-        && libro.título.trim().split(" ").length > 1) //se asegura de que el título tenga más de una palabra
-        .map(l => l.título); //Devuelve un nuevo array con solo los títulos de los libros que cumplieron la condición
+        .filter(libro => /^[a-zA-Z\s]+$/.test(libro.titulo) //filtra los libros cuyo título contiene solo letras y espacios 
+        && libro.titulo.trim().split(" ").length > 1) //se asegura de que el título tenga más de una palabra
+        .map(l => l.titulo); //Devuelve un nuevo array con solo los títulos de los libros que cumplieron la condición
     console.log(resultado);
     return resultado;
 }
