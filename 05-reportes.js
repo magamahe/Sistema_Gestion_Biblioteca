3f61c0eb4fab72887a44d207ff070cf3118be699
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-06-28 18:07:39 -0300

    cambio de impresion de  lista de libros por generos

--- 05-reportes.js
+++ 05-reportes.js
@@ -1,25 +1,25 @@
 /* PUNTO 5: Reportes */
 
-
-const libros = require("./01-lista_libros.js");
+const { biblioteca } = require("./02-gestion_libro");
 
 function generarReporteLibros(){
-    const totalLibros = libros.length; // calcula la cantidad total de libros existentes
-    const librosPrestados = libros.filter(libro => !libro.disponible).length; // calcular la cantidad de libros prestados
-    const librosPorGenero = libros.reduce((acc, libro) => {
+    const totalLibros = biblioteca.length; // calcula la cantidad total de libros existentes
+    const librosPrestados = biblioteca.filter(libro => !libro.disponible).length; // calcular la cantidad de libros prestados
+    const librosPorGenero = biblioteca.reduce((acc, libro) => {
                             acc[libro.genero] = (acc[libro.genero] || 0) + 1;
                             return acc;
     }, {}); // Calcula la cantidad de Libro por genero
-    const libroMasAntiguo = libros.reduce((acum, libro) => (acum.año < libro.año ? acum : libro));
-    const libroMasNuevo = libros.reduce((acum, libro) => (acum.año > libro.año ? acum : libro));
+    const libroMasAntiguo = biblioteca.reduce((acum, libro) => (acum.anio < libro.anio ? acum : libro));
+    const libroMasNuevo = biblioteca.reduce((acum, libro) => (acum.anio > libro.anio ? acum : libro));
 
     //Reporte de Libros
     console.log("📚📋 REPORTE DE LIBROS 📋📚");
     console.log(`📘 Cantidad Total de libros: ${totalLibros}`);
     console.log(`📕 Cantidad de libros Prestados: ${librosPrestados}`);
-    console.log(`📗 Cantidad de libros por Género: ${librosPorGenero}`);
-    console.log(`📙 Libro más Antiguo: ${libroMasAntiguo.título} , 📅 Año: ${libroMasAntiguo.año}`);
-    console.log(`📒 Libro más Nuevo: ${libroMasNuevo.título}, 📅 Año: ${libroMasNuevo.año}`);    
+    console.log(`📗 Cantidad de libros por Género:`);
+    console.table(librosPorGenero);
+    console.log(`📙 Libro más Antiguo: ${libroMasAntiguo.titulo} , 📅 Año: ${libroMasAntiguo.anio}`);
+    console.log(`📒 Libro más Nuevo: ${libroMasNuevo.titulo}, 📅 Año: ${libroMasNuevo.anio}`);    
 }
 
 module.exports = {
