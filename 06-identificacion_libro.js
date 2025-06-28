b65d88827f7cea8a24968cb831228136c34b8e7f
Antonela Borgogno <antonelaborgogno@gmail.com>
2025-06-28 18:08:06 -0300

    quitar console.log de resultado para imprimirlo en formato tabla en 09-menu

--- 06-identificacion_libro.js
+++ 06-identificacion_libro.js
@@ -7,7 +7,6 @@ function librosConPalabrasEnTitulo(){
         .filter(libro => /^[a-zA-Z\s]+$/.test(libro.titulo) //filtra los libros cuyo título contiene solo letras y espacios 
         && libro.titulo.trim().split(" ").length > 1) //se asegura de que el título tenga más de una palabra
         .map(l => l.titulo); //Devuelve un nuevo array con solo los títulos de los libros que cumplieron la condición
-    console.log(resultado);
     return resultado;
 }
 
